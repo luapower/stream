465e428ce1b977cc6efaaba3a83e732268afc0d1
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2019-11-26 19:06:58 +0200

    unimportant

--- stream.lua
+++ stream.lua
@@ -58,10 +58,10 @@ function stream.linebuffer(read, term, sz)
 
 	local find_term
 	if #term == 1 then
-		term = string.byte(term)
+		local t = string.byte(term)
 		function find_term(buf, i, j)
 			for i = i, j-1 do
-				if buf[i] == term then
+				if buf[i] == t then
 					return true, i, i+1
 				end
 			end
@@ -70,7 +70,7 @@ function stream.linebuffer(read, term, sz)
 	elseif #term == 2 then
 		local t1, t2 = string.byte(term, 1, 2)
 		function find_term(buf, i, j)
-			for i = i, j-1 do
+			for i = i, j-2 do
 				if buf[i] == t1 and buf[i+1] == t2 then
 					return true, i, i+2
 				end
@@ -86,27 +86,29 @@ function stream.linebuffer(read, term, sz)
 	assert(sz >= 1024)
 	local buf = ffi.new('char[?]', sz)
 
-	local i, j = 0, 0
+	local i = 0 --index of first valid byte.
+	local j = 0 --index right after last valid byte.
 
 	local function more()
-		if j + 128 >= sz then
-			if j == sz and i == 0 then
-				print(debug.traceback())
+		if j == i then --buffer empty: reset.
+			i, j = 0, 0
+		elseif j == sz then --no more space at the end.
+			if i == 0 then --buffer full.
 				return nil, 'line too long'
-			else --move partial line to the beginning.
+			else --move data to make space at the end.
 				ffi.copy(buf, buf + i, j - i)
 				i, j = 0, j - i
 			end
 		end
 		local n, err = read(buf + j, sz - j)
 		if not n then return nil, err end
-		if n == 0 then return more() end
+		if n == 0 then return nil, 'null read' end
 		j = j + n
 		return true
 	end
 
 	local function readline()
-		if j == i then
+		if j == i then --buffer empty: refill.
 			local ok, err = more()
 			if not ok then return nil, err end
 		end
@@ -118,7 +120,7 @@ function stream.linebuffer(read, term, sz)
 				i = next_i
 				return s
 			else
-				n = j - i
+				n = j - i - (#term - 1)
 				local ok, err = more()
 				if not ok then return nil, err end
 			end
@@ -126,7 +128,7 @@ function stream.linebuffer(read, term, sz)
 	end
 
 	local function read(maxn)
-		if j == i then
+		if j == i then --buffer empty: refill.
 			local ok, err = more()
 			if not ok then return nil, err end
 		end
--- stream_test.lua
+++ stream_test.lua
@@ -4,29 +4,57 @@ local clock = require'time'.clock
 local pp = require'pp'
 
 local function linebuffer_fuzz_test()
-	local seed = math.floor(clock())
-	--local seed = 2450522
-	--local seed = 2450538
-	--local seed = 2450565
-	--local seed = 2450639
+	local seed = math.floor(clock() * 10)
 	print('randomseed', seed)
 	math.randomseed(seed)
 
-	local max_line_size = 1024 * 2
-	local t = {}
-	for i = 1, math.random(max_line_size * math.pi * 1000) do
-		t[i] = string.char(math.random(0, 255))
-	end
-	local s = table.concat(t) .. '\r\n'
+	local max_line_size = 1024 + math.random(1024 * 8)
+	local total_size = math.random(max_line_size * math.pi * 100)
+	local max_read_size = math.random(total_size)
+	local term_r = math.random() > .5
+	local term_n = math.random() > .5
+	if not (term_r or term_n) then term_n = true end
+	local term =
+		(term_r and '\r' or '') ..
+		(term_n and '\n' or '')
 
-	local n1, n2 = 0, 1/0
-	for s in s:gmatch'[^\r\n]+' do
-		n1 = math.max(n1, #s)
-		n2 = math.min(n2, #s)
+	--generate random buffer
+	local t = {}
+	for i = 1, total_size do
+		local c = string.char(math.random(0, 255))
+		if c ~= '\r' and c ~= '\n' then
+			t[#t+1] = c
+		end
 	end
-	local term = s:match'\r?\n'
-	print('line size range: '..n2..'..'..n1..
-		' (max='..max_line_size..') term: '..pp.format(term))
+
+	--generate line breaks at random positions in the buffer.
+	local i = 1
+	local n = #t
+	while true do
+		i = i + math.random(max_line_size)
+		if i+1 >= n then break end
+		if term_r and term_n then
+			t[i] = '\r'
+			t[i+1] = '\n'
+		else
+			t[i] = term_r and '\r' or '\n'
+		end
+	end
+	local s = table.concat(t, nil) .. term
+
+	--analyze line sizes.
+	local n1, n2, sum, count = 0, 1/0, 0, 0
+	for i1, i2 in s:gmatch('().-()'..term) do
+		local n = i2 - i1
+		n1 = math.max(n1, n)
+		n2 = math.min(n2, n)
+		sum = sum + n
+		count = count + 1
+	end
+	local avg = math.floor(sum / count + .5)
+	print(string.format('line %d..%d, avg %d, count %d, max %d, term=%s',
+			n2, n1, avg, count, max_line_size, pp.format(term)))
+	print(string.format('total %d, max read %d', total_size, max_read_size))
 
 	local read = stream.memreader(s)
 	local read = stream.readtobuffer(read)
@@ -35,12 +63,13 @@ local function linebuffer_fuzz_test()
 	local t = {}
 	while true do
 		local s, err
-		if math.random() > .5 then
+		if math.random() > math.random() then
 			s, err = lb.readline()
 			if not s then break end
 			s = s .. term
 		else
-			local buf, sz = lb.read(math.random(max_line_size * math.pi / 2))
+			local maxsize = math.random(max_read_size)
+			local buf, sz = lb.read(maxsize)
 			if not buf then break end
 			s = ffi.string(buf, sz)
 		end
@@ -52,4 +81,7 @@ local function linebuffer_fuzz_test()
 	assert(s2 == s)
 end
 
-linebuffer_fuzz_test()
+for i = 1, 10000 do
+	print(i)
+	linebuffer_fuzz_test()
+end
